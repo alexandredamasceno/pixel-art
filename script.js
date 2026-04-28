1c554dd26b069a2db080c131ee9726f60ceb9612
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2026-04-27 21:12:16 -0300

    Cria lógica para renderizar cores aleatórias quando se recarrega a página e inicia a refatoração do código

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// Cria Paleta de Cores
-const getTheColorPalete = document.querySelector("#color-palette");
-const arrayColorsPalette = ["black", "blue", "green", "red"];
-let colorPaleteLength = 4;
-
-for (let index = 0; index < colorPaleteLength; index += 1) {
-  let createColor = document.createElement("div");
-  if (arrayColorsPalette[index] === "black") {
-    createColor.className = "color selected";
-  } else {
-    createColor.className = "color";
+// Função que gera cores aleatória ao carregar a página
+const generateRandomColor = (length) => {
+  let newColors = ["rgb(0 0 0)", "#FFFFFF"];
+  let initialNumber;
+  let middleNumber;
+  let lastNumber;
+  for (let index = 0; index < length - 2; index += 1) {
+    initialNumber = Math.floor(Math.random() * 256);
+    middleNumber = Math.floor(Math.random() * 256);
+    lastNumber = Math.floor(Math.random() * 256);
+    let newRGB = `rgb(${initialNumber} ${middleNumber} ${lastNumber})`;
+    newColors.push(newRGB);
   }
-  createColor.style.backgroundColor = arrayColorsPalette[index];
-  getTheColorPalete.appendChild(createColor);
+
+  return newColors;
 };
-//
 
-//Função que cria o quadro de pixels para pintar
-function createPixelBoard(boardWidth, boardHeight) {
-  if ((boardHeight < 5 || boardWidth < 5) || (boardHeight > 20 || boardWidth > 20)) {
-    return alert("Largura ou comprimento inválido! Largura e Comprimento máximo de 20 unidades e mínimo de 5 unidades");
-  }
-  const getThePixelBoard = document.querySelector("#pixel-board");
-  getThePixelBoard.innerHTML = "";
-  for (let indexH = 0; indexH < boardHeight; indexH += 1) {
-    let createTableLine = document.createElement("div");// cria o comprimento
-    for (let indexW = 0; indexW < boardWidth; indexW += 1) {
-      let createTableCell = document.createElement("div");// cria a largura
-      createTableCell.className = "pixel";
-      createTableLine.appendChild(createTableCell);
-    }
-    getThePixelBoard.appendChild(createTableLine);
-  }
+// Cria Paleta de Cores
+function createColorsPalette() {
+  const getTheColorPalette = document.querySelector("#color-palette");
+  const colorPaletteLength = 10;
+  let arrayColorsPalette = generateRandomColor(colorPaletteLength);
+
+  for (let index = 0; index < arrayColorsPalette.length; index += 1) {
+    let createColor = document.createElement("div");
+    createColor.classList.add("color");
+    if (arrayColorsPalette[index] === "rgb(0 0 0)") {
+      createColor.classList.add("selected");
+    } 
+    createColor.style.backgroundColor = arrayColorsPalette[index];
+    getTheColorPalette.appendChild(createColor);
+  };
 }
+//
 
 // Lógica para adicionar a classe selected unicamente na cor escolhida da paleta de cores para depois buscarmos essa cor selecionado(com a class selected)
 let paletteColors = document.getElementsByClassName("color");
@@ -43,20 +43,43 @@ const selectColorToUse = (event) => {
   event.target.className = "color selected";
 };
 // Adiciona eventos em todas as paletas de cores
-for (let index = 0; index < paletteColors.length; index += 1) {
-  paletteColors[index].addEventListener("click", selectColorToUse);
+function addEventsToEachColorInPalette() {
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    paletteColors[index].addEventListener("click", selectColorToUse);
+  }
 }
 //
+
+//Função que cria o quadro de pixels para pintar
+function createPixelBoard(boardWidth, boardHeight) {
+  //Validação da largura e comprimento
+  if ((boardHeight < 5 || boardWidth < 5) || (boardHeight > 20 || boardWidth > 20)) {
+    return alert("Largura ou comprimento inválido! Largura e Comprimento máximo de 20 unidades e mínimo de 5 unidades");
+  }
+  const getThePixelBoard = document.querySelector("#pixel-board");
+  getThePixelBoard.innerHTML = "";
+  for (let indexH = 0; indexH < boardHeight; indexH += 1) {
+    let createDivFather = document.createElement("div");// cria o comprimento
+    for (let indexW = 0; indexW < boardWidth; indexW += 1) {
+      let createChildDiv = document.createElement("div");// cria a largura
+      createChildDiv.className = "pixel";
+      createDivFather.appendChild(createChildDiv);
+    }
+    getThePixelBoard.appendChild(createDivFather);
+  }
+}
 
 let getAllPixels = document.getElementsByClassName("pixel");
 
 //Cria a lógica para pintar cada pixel com a cor escolhida na paleta de cores(com a class selected)
+
+// Pinta com a cor do elemento que tem a classe .selected o pixel que foi alvo do evento(target)
+function addColorToPixel(event) {
+  let getSelectedColor = document.querySelector(".selected");
+  event.target.style.backgroundColor = getSelectedColor.style.backgroundColor;
+}
+
 function addEventsToEachPixel() {
-  // Pinta com a cor do elemento que tem a classe .selected o pixel que foi alvo do evento(target)
-  function addColorToPixel(event) {
-    let getSelectedColor = document.querySelector(".selected");
-    event.target.style.backgroundColor = getSelectedColor.style.backgroundColor;
-  }
   // Adiciona evento de click em todas as divs que tem class Pixel
   for (let index = 0; index < getAllPixels.length; index += 1) {
     getAllPixels[index].addEventListener("click", addColorToPixel);
@@ -83,6 +106,8 @@ getInputButton.addEventListener("click", () => {
 });
 
 window.onload = () => {
+  createColorsPalette();
+  addEventsToEachColorInPalette();
   createPixelBoard(5, 5);
   addEventsToEachPixel();
 };
